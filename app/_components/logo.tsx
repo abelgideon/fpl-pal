e2f616396cbbac77be57be96e14f7b21b53ee831
Abel Gideon <abelgideontk7@gmail.com>
2025-11-09 19:05:05 +0300

    fix: Update logo to use tailwind

--- app/_components/logo.tsx
+++ app/_components/logo.tsx
@@ -1,8 +1,8 @@
 export function Logo() {
   return (
     <p className="text-4xl" style={{ fontFamily: "fantasy" }}>
-      <span style={{ color: "#933dff" }}>FPL</span>
-      <span style={{ color: "#31d684" }}>pal</span>
+      <span className="text-[#933dff]">FPL</span>
+      <span className="text-[#31d684]">pal</span>
     </p>
   );
 }
